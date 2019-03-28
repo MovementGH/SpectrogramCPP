f42681c95ea208ad2833e620aa4ffdb6b62b69e2
Movement <>
2019-03-27 19:21:51 -0700

    Create Project

---
+++ Source/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SpectrographCPP
+//
+//  Created by Mayo Furgerson on 3/27/19.
+//  Copyright © 2019 WimMa Games. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
